c9326e3311d8da66829f26242829fd34e5d699e3
Elvis-rozy <elvisrozy52@gmail.com>
2023-09-05 10:43:31 +0000

    task done

--- 0x04-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -1,55 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, css } from "aphrodite";
+import { StyleSheet, css } from 'aphrodite';
 
-
-class NotificationItem extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.selected_style = this.props.type === 'default' ?  itemStyles.default : itemStyles.urgent;
-  }
-
-  render() {
-    return (
-      this.props.value ? 
-      <li
-      data-notification-type={this.props.type}
-      onClick={() => this.props.markAsRead(this.props.id)}
-      className={css(this.selected_style)}
-      >{this.props.value}</li> 
-      :
-      <li
-      data-notification-type={this.props.type}
-      dangerouslySetInnerHTML={this.props.html}
-      onClick={() => {console.log('empty func');}}
-      className={css(this.selected_style)}
-      ></li>
-    );
-  }
-};
-
-const itemStyles = StyleSheet.create({
-  urgent: {
-		color: 'red'
-	},
-
-	default: {
-		color: 'blue'
-	}
+const styles = StyleSheet.create({
+    default: {
+        color: 'blue',
+    },
+    urgent: {
+        color: 'red',
+    },
 });
 
-NotificationItem.defaultProps = {
-  type: 'default',
-  markAsRead: () => {console.log('empty func');},
-	id: 0
-};
+class NotificationItem extends React.PureComponent {
+    render() {
+        const { type, value, html, markAsRead, id } = this.props;
+        return (
+            <React.Fragment>
+                {type && value ? (
+                    <li
+                        className={
+                            type === 'default' ? css(styles.default) : css(styles.urgent)
+                        }
+                        onClick={() => markAsRead(id)}
+                        data-notification-type={type}
+                    >
+                        {value}
+                    </li>
+                ) : null}
+                {html ? (
+                    <li
+                        onClick={() => markAsRead(id)}
+                        data-urgent
+                        className={css(styles.urgent)}
+                        dangerouslySetInnerHTML={{ __html: html }}
+                    ></li>
+                ) : null}
+            </React.Fragment>
+        );
+    }
+}
 
 NotificationItem.propTypes = {
-  html: PropTypes.shape({__html: PropTypes.string}),
-  type: PropTypes.string.isRequired,
-  value: PropTypes.string,
-  markAsRead: PropTypes.func,
-  id: PropTypes.number
+    type: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    __html: PropTypes.shape({
+        html: PropTypes.string,
+    }),
+};
+
+NotificationItem.defaultProps = {
+    type: 'default',
 };
 
 export default NotificationItem;
--- 0x04-React_inline_styling/task_4/dashboard/src/BodySection/BodySection.js
+++ 0x04-React_inline_styling/task_4/dashboard/src/BodySection/BodySection.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class BodySection extends Component {
-  render() {
+const BodySection = ({ title, children }) => {
     return (
-      <div className='bodySection'>
-        <h2>{this.props.title}</h2>
-        {this.props.children}
-      </div>
+        <div className='bodySection'>
+            <h2>{title}</h2>
+            {children}
+        </div>
     );
-  }
-}
-
-BodySection. defaultProps = {
-	children: <React.Fragment />
 };
 
 BodySection.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.element
-  ])
+    title: PropTypes.string,
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]).isRequired,
 };
 
 export default BodySection;
